68793e83dd7bfa02f6179df134296be899d5a3bd
Sean Bode <seanpaulbode@gmail.com>
2016-08-10 15:29:21 -0400

    markdown fixed

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,6 +1,6 @@
 module WikisHelper
-  def markdown(markdown)
-    renderer = Redcarpet::Markdown.new(footnote: true, quote: true, autolink: true, tables: true)
-    renderer.render(markdown)
+  def markdown_to_html(mdown)
+    @renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, footnote: true, quote: true, autolink: true, tables: true)
+    @renderer.render(mdown).html_safe
   end
 end
